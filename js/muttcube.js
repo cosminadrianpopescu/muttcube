10072a69427c541232da41e8681dc625f7272e00
Cosmin Popescu <cosminadrianpopescu@gmail.com>
2015-09-08 20:34:05 +0200

    working

--- js/muttcube.js
+++ js/muttcube.js
@@ -275,6 +275,7 @@ var muttcube = {
                 if (this.from_insert){
                     this.from_insert = false;
                     muttcube.commands.change_mode(this.prev_mode());
+                    $('#muttcube-command-input').hide();
                 }
                 else {
                     $('#muttcube-statusbar-message').hide();
